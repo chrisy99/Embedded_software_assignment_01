20e1390a5a93a32179013791531ed3c8ba585066
Chris <chrisyip82@gmail.com>
2023-03-03 14:43:30 +0000

    added methods

--- Assignment_2/Assignment_2.ino
+++ Assignment_2/Assignment_2.ino
@@ -2,6 +2,7 @@
 
 /* Sweep
  by BARRAGAN <http://barraganstudio.com>
+ 
  This example code is in the public domain.
 
  modified 8 Nov 2013
@@ -9,25 +10,35 @@
  http://www.arduino.cc/en/Tutorial/Sweep
 */
 
-#include <Servo.h>
+B31DGCyclicExecutiveMonitor monitor;
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+void setup()
+{
+  monitor.starMonitoring();
 }
 
-void loop() {
-   for (pos = 0; pos <= 180; pos += 2) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 2) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+void PerformInstanceOfTask1()
+{
+  monitor.jobStarted(1);
+
+  // Do something here
+
+  monitor.jobEnabled(1);
 }
+
+void loop(void)
+{
+  // Cyclic cycle
+}
+
+/*
+ * Task 1:
+ * high
+ * 200 micros pause
+ * low
+ * 50 micor seconds
+ * high 
+ * 30 micro secnonds
+ * 
+ * period 4ms/ 250hz
+ */
